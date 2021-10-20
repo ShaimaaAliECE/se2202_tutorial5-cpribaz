06e525ded83b9b129c2858bbba19a2bbf62ef96c
Connor Pribaz <cpribaz@uwo.ca>
2021-10-19 20:51:23 -0400

    Filled in methods with code

--- scripts.js
+++ scripts.js
@@ -1,9 +1,17 @@
 let nextPlayer = 'X'; // takes a value of either 'X' or 'O' according to the game turns
 
 //initialize the game
+var initializeBtn = document.createElement('button');
+initializeBtn.innerText = 'Click here to start a new game';
+document.getElementById('game-over-lbl').appendChild(initializeBtn);
+initializeBtn.addEventListener('click', (initialEvent) => {initialEvent.target.hidden = true;});
 
 // use the value stored in the nextPlayer variable to indicate who the next player is
-
+let player1 = 'Y';
+let player2 = 'X';
+let nextLabel = document.getElementById('next-lbl');
+nextLabel.innerText = nextPlayer;
+let counter = 0;
 
 //This call will create the buttons needed for the gameboard.
 createGameBoard()
@@ -11,7 +19,11 @@ createGameBoard()
 function createGameBoard()
 {
     // Programatically add a button with square brackets enclosing an empty space to each cell in the gameboard
-   
+   for (let i=0; i<9;i++){
+       let cellID = 'c' + (i+1);
+       var newBtn = document.createElement('button');
+       document.getElementById(cellID).appendChild(newBtn);
+   }
 }
 
 // Programatically add 'takeCell' as an event listener to all the buttons on the board
@@ -27,6 +39,16 @@ function takeCell(event)
     /*
         When the button is clicked, the space inside its square brackets is replaced by the value in the nextPlayer before switching it
     */
+   let clicked = event.target;
+   clicked.innerText = nextPlayer;
+   clicked.disable = true;
+   if (nextPlayer==='X'){
+       nextPlayer = 'Y';
+   }else if (nextPlayer==='Y'){
+       nextPlayer = 'X';
+   }
+   nextLabel.innerText = nextPlayer;
+   counter++;
 
     // Make sure the button is clickable only once (I didn't mention how to do that, look it up :) )
 
@@ -34,6 +56,8 @@ function takeCell(event)
     if (isGameOver())
     {
         // let the lable with the id 'game-over-lbl' display the words 'Game Over' inside <h1> element
+        let gameOver = document.getElementById('game-over-lbl');
+        nextLabel.innerHTML = '<h1>GameOver</h1>'
     }
 
     // I'll leave declaring the winner for your intrinsic motivation, it's not required for this assignment 
@@ -42,5 +66,5 @@ function takeCell(event)
 function isGameOver()
 {
     // This function returns true if all the buttons are disabled and false otherwise 
-   
+   return counter==9
 }
